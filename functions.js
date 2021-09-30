7539e77ceca26625e1233218c6bd95bc8d181456
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-09-30 10:03:34 -0700

    added notes about functions

---
+++ functions.js
@@ -0,0 +1,19 @@
+// Anything with () after it is a function call
+console.log(Math.random());
+console.log(Date.now());
+
+console.log('information to log');
+console.log(Math.floor(5.39))
+
+// console.log is a function
+
+const myRandomNum = Math.random();
+console.log(myRandomNum);
+
+// return value
+
+function helloWorld(){
+    return 'Hello world';
+}
+const hello = helloWorld();
+console.log(hello);
